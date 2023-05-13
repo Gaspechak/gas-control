420de9240967442b4fb6fb9ca1d544968a5c29db
Gaspechak <mathgaspechak@outlook.com>
2023-05-13 17:14:39 -0400

    Adicionar autoria

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const formatarValorMonetario = (valor) => {
 }
 
 export default function Home() {
+    console.log("Powered by Matheus Gaspechak")
     const { data, error } = useSWR('/api/dashboard', fetcher, { refreshInterval: 100 })
 
     if (error) return <div>Failed to Load</div>
